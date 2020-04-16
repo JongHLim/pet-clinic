69d50280ea7d650fdf8fedeea765579ffa7633b3
Jon Lim <jon.lim@utexas.edu>
2020-04-15 20:41:00 -0600

    Implement Map based services. Closes issue #6.

--- pet-clinic-data/src/main/java/me/jonlim/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/me/jonlim/petclinic/services/PetService.java
@@ -2,8 +2,6 @@ package me.jonlim.petclinic.services;
 
 import me.jonlim.petclinic.model.Pet;
 
-import java.util.Set;
-
 public interface PetService extends CrudService<Pet, Long> {
 
 }
--- pet-clinic-data/src/main/java/me/jonlim/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/me/jonlim/petclinic/services/VetService.java
@@ -2,8 +2,6 @@ package me.jonlim.petclinic.services;
 
 import me.jonlim.petclinic.model.Vet;
 
-import java.util.Set;
-
 public interface VetService extends CrudService<Vet, Long> {
 
 }
