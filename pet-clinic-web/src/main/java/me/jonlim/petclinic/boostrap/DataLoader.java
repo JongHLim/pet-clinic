dbea275ebb357823229bcc0c9e2b8a2e73403e2d
Jon Lim <jon.lim@utexas.edu>
2020-04-26 14:33:48 -0600

    Added PetTypes to bootstrap (data loader). closes #28

--- pet-clinic-web/src/main/java/me/jonlim/petclinic/boostrap/DataLoader.java
+++ pet-clinic-web/src/main/java/me/jonlim/petclinic/boostrap/DataLoader.java
@@ -1,8 +1,10 @@
 package me.jonlim.petclinic.boostrap;
 
 import me.jonlim.petclinic.model.Owner;
+import me.jonlim.petclinic.model.PetType;
 import me.jonlim.petclinic.model.Vet;
 import me.jonlim.petclinic.services.OwnerService;
+import me.jonlim.petclinic.services.PetTypeService;
 import me.jonlim.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,24 @@ public class DataLoader implements CommandLineRunner {
 
   private final OwnerService ownerService;
   private final VetService vetService;
+  private final PetTypeService petTypeService;
 
-  public DataLoader(OwnerService ownerService, VetService vetService) {
+  public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
     this.ownerService = ownerService;
     this.vetService = vetService;
+    this.petTypeService = petTypeService;
   }
 
   @Override
   public void run(String... args) throws Exception {
+
+    PetType dog = new PetType();
+    dog.setName("Dog");
+    PetType savedDogType = petTypeService.save(dog);
+
+    PetType cat = new PetType();
+    dog.setName("Cat");
+    PetType savedCatType = petTypeService.save(cat);
 
     Owner owner1 = new Owner();
     owner1.setFirstName("Jon");
