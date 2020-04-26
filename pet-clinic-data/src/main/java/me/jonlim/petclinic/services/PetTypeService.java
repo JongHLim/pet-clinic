ebdf38e536f3e9e8026e6fbfd727b299ae4e5720
Jon Lim <jon.lim@utexas.edu>
2020-04-26 14:28:10 -0600

    Create PetType service. closes #26

---
+++ pet-clinic-data/src/main/java/me/jonlim/petclinic/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package me.jonlim.petclinic.services;
+
+import me.jonlim.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+
+
+}
---
+++ pet-clinic-data/src/main/java/me/jonlim/petclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,36 @@
+package me.jonlim.petclinic.services.map;
+
+import me.jonlim.petclinic.model.PetType;
+import me.jonlim.petclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+  @Override
+  public Set<PetType> findAll() {
+    return super.findAll();
+  }
+
+  @Override
+  public PetType findById(Long id) {
+    return findById(id);
+  }
+
+  @Override
+  public PetType save(PetType object) {
+    return super.save(object);
+  }
+
+  @Override
+  public void delete(PetType object) {
+    super.delete(object);
+  }
+
+  @Override
+  public void deleteById(Long id) {
+    super.deleteById(id);
+  }
+}
