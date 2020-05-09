8749fd1c5c8f775cbb71b1186ad402f3513e442e
Jon Lim <jon.lim@utexas.edu>
2020-05-09 13:13:43 -0600

    updated owner with JPA entity mappings. Closes #32

--- pet-clinic-data/src/main/java/me/jonlim/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/me/jonlim/petclinic/model/Pet.java
@@ -1,12 +1,24 @@
 package me.jonlim.petclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
 
+  @ManyToOne()
+  @JoinColumn(name = "type_id")
   private PetType petType;
+
+  @ManyToOne
+  @JoinColumn(name = "owner_id")
   private Owner Owner;
+
+  @Column(name="birth_date")
   private LocalDate birthDate;
+
+  @Column(name = "name")
   private String name;
 
   public String getName() {
